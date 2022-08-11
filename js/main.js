8595880be57047fa888b409b3c11ad7bbc6a3040
marramags <marra.emata@gmail.com>
2022-08-11 16:05:43 -0700

    added gravity

--- js/main.js
+++ js/main.js
@@ -4,11 +4,22 @@ const results = document.getElementById('results');
 const currPlayer = document.getElementById('currPlayer')
 let playerTurn = 1;
 
-for (let i = 0; i < circles.length; i++) {
-    circles[i].addEventListener('click', () => {
-        if (circles[i +7].classList.contains('.filled') && !circles[i].classList.contains('.filled'))
+for (let i = 0; i < circles.length; i++){
+        circles[i].addEventListener('click', () => {
+        if (circles[i +7].classList.contains('filled') && !circles[i].classList.contains('filled')) {
             if (playerTurn == 1) {
-                
+                circles[i].classList.add('filled')
+                circles[i].classList.add('player1')
+                playerTurn = 2
+                currPlayer.innerHTML = playerTurn
+            } else if (playerTurn == 2) {
+                circles[i].classList.add('filled')
+                circles[i].classList.add('player2')
+                playerTurn == 1
+                currPlayer.innerHTML = playerTurn
             }
-    })
-}
+    } else
+        results.innerHTML = "nope"   
+        }) 
+        
+    };
