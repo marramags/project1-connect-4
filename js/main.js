313da978729895e31d06702bcdcb1f008a69afe8
marramags <marra.emata@gmail.com>
2022-08-11 15:51:19 -0700

    added event listener to circles & adjusted class names

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@
 const circles = document.querySelectorAll('.gameBoard div');
 const results = document.getElementById('results');
 const currPlayer = document.getElementById('currPlayer')
+let playerTurn = 1;
+
+for (let i = 0; i < circles.length; i++) {
+    circles[i].addEventListener('click', () => {
+        if (circles[i +7].classList.contains('.filled') && !circles[i].classList.contains('.filled'))
+            if (playerTurn == 1) {
+                
+            }
+    })
+}
