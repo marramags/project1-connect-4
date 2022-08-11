ce8eeb2f71553beff84cd4ee90593089747fb3da
marramags <marra.emata@gmail.com>
2022-08-11 15:43:11 -0700

    grabbed elements & updated HTML&CSS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
 // alert('I can do this!')
+const circles = document.querySelectorAll('.gameBoard div');
+const results = document.getElementById('results');
+const currPlayer = document.getElementById('currPlayer')
