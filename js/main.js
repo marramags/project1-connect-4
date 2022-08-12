e18dcef4d7b031c2a799c12a7473b74916a0fe5e
marramags <marra.emata@gmail.com>
2022-08-11 17:25:40 -0700

    added horizontal winnings

--- js/main.js
+++ js/main.js
@@ -8,40 +8,53 @@ const winningCombos =[
     [0, 7, 14, 21],
     [7, 14, 21, 28],
     [14, 21, 28, 35],
-
     [1, 8, 15, 22],
     [8, 15, 22, 29,],
     [15, 22, 29, 36],
-
     [2, 9, 16, 23],
     [9, 16, 23, 30],
     [16, 23, 30, 37],
-
     [3, 10, 17, 25],
     [10, 17, 25, 31],
     [17, 25, 31, 38],
-
     [4, 11, 18, 25],
     [11, 18, 25, 32],
     [18, 25, 32, 39],
-
     [5, 12, 19, 26],
     [12, 19, 26, 33],
     [19, 26, 33, 40],
-
     [6, 13, 20, 27],
     [13, 20, 27, 34],
     [20, 27, 34, 41],
+
+    [0, 1, 2, 3],
+    [1, 2, 3, 4],
+    [2, 3, 4, 5],
+    [3, 4, 5, 6],
+
+    [7, 8, 9, 10],
+    [8, 9, 10, 11],
+    [9, 10, 11, 12],
+    [10, 11, 12, 13],
+    
+    [14, 15, 16, 17],
+    [15, 16, 17, 18],
+    [16, 17, 18, 19],
+    [17, 18, 19, 20],
+
+    [21],
 ]
 
 
 function checkWinner() {
-    // winningCombos.forEach(space => {
-    //     const circle1 = circles[winningCombos[space][0]
-    //     ]
-    // })
+    winningCombos.forEach((space, idx) => {
+        const circle1 = circles[space,[idx]]
+        console.log(circle1)
+
+    })
     for (let i = 0; i < winningCombos.length; i++){
         const circle1 = circles[winningCombos[i][0]]
+        console.log(circle1)
         const circle2 = circles[winningCombos[i][1]]
         const circle3 = circles[winningCombos[i][2]]
         const circle4 = circles[winningCombos[i][3]]
